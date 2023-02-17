17fff19df6908435e6bfca6f21119aef411cd673
Mathieu Tourte <Tourte.Mathieu@gmail.com>
2023-02-17 17:14:52 +0100

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import ProfileCard from "./components/ProfileCard/ProfileCard";
 function App() {
   return (
     <div className="App">
-      <ProfileCard profilID="76561198042858555" />
-      <GameList profilID="76561198042858555" />
+      <ProfileCard profilID="" />
+      <GameList profilID="" />
     </div>
   );
 }
